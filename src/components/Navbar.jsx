5653461273e5d4aedd30e40c4e195e29fd3d0597
Alex <alex.holder@bigfootds.com>
2024-06-12 20:55:20 +1000

    feat: rework navbar content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,28 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
+import ArticleMeta from "../articleMeta.jsx";
+import { useEffect, useState } from "react";
+import { readingTime } from "reading-time-estimator";
 
 
 export default function Navbar(){
+	let [estimatedReadingTime, setEstimatedReadingTime] = useState({});
+	let location = useLocation();
+	let [articleRouteName, setArticleRouteName] = useState("");
+
+	useEffect(() => {
+		setArticleRouteName(location.pathname.substring(location.pathname.lastIndexOf("/") + 1));
+
+		document.getElementById("positionHelper").scrollTo({top: 0, left: 0, behavior: "smooth"});
+	}, [location]);
+
+	useEffect(() => {
+		if (articleRouteName && ArticleMeta[articleRouteName]){
+			document.title = ArticleMeta[articleRouteName].title + " | Alex Stormwood";
+			// console.log(ArticleMeta[articleRouteName]);
+			setEstimatedReadingTime(readingTime(document.getElementById("positionHelper").textContent));
+		}
+	}, [articleRouteName]);
 
 	return (
 		<nav>
@@ -12,6 +32,10 @@ export default function Navbar(){
 						<img src="/android-chrome-384x384.png" height={"40"} alt="Photo of me - Alex!" />
 						<h1>Alex Stormwood</h1>
 					</Link>
+				</div>
+				<div id="article-info">
+					<h1>{ArticleMeta[articleRouteName]?.title}</h1>
+					<p>{estimatedReadingTime?.text}</p>
 				</div>
 				<div className="navbar-links">
 					<NavLink className={"navButton"} to={"/"}>Home</NavLink>
--- src/pages/_ArticleLayout.jsx
+++ src/pages/_ArticleLayout.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import useScrollPercentage from "react-scroll-percentage-hook";
-import { readingTime } from "reading-time-estimator";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/ArticleLayout.css";
@@ -17,7 +16,6 @@ function ArticleLayout() {
 	const { ref, percentage } = useScrollPercentage();
 	let location = useLocation();
 	let [articleRouteName, setArticleRouteName] = useState("");
-	let [estimatedReadingTime, setEstimatedReadingTime] = useState({});
 	let dateFormatOptions = {
 		month: "long",
 		day: "numeric",
@@ -39,7 +37,7 @@ function ArticleLayout() {
 		if (articleRouteName){
 			document.title = ArticleMeta[articleRouteName].title + " | Alex Stormwood";
 			// console.log(ArticleMeta[articleRouteName]);
-			setEstimatedReadingTime(readingTime(document.getElementById("positionHelper").textContent));
+			// setEstimatedReadingTime(readingTime(document.getElementById("positionHelper").textContent));
 			setScrollPilotConfig({
 				index: articleToc,
 				aesthetics: {
@@ -49,7 +47,7 @@ function ArticleLayout() {
 					indicatorProgressColor:"var(--theme-100)",
 					hideOnScroll: true,
 					margins: {
-						desktop: 0,
+						desktop: 10,
 						mobile: 0
 					},
 					popupWidths: {
@@ -64,13 +62,12 @@ function ArticleLayout() {
 	return (<>
 
 		<div id="root-container">
-			<ScrollPilot config={scrollPilotConfig} />
+			<div id="tocHelper">
+				<ScrollPilot className="scrollPilotManualStyles" config={scrollPilotConfig} />
+			</div>
 			<Navbar />
 			<div id="article-scrollprogress">
-				<div id="article-info">
-					<h1>{ArticleMeta[articleRouteName]?.title}</h1>
-					<p>{estimatedReadingTime?.text}</p>
-				</div>
+
 				
 				<div id="customScrollProgressBackground" style={{backgroundColor: "var(--theme-500)",height: "5px", width: "100%"}}>
 					<div id="customScrollProgressForeground" style={{backgroundColor: "var(--theme-100)",height: "100%", width: `${percentage.vertical}%`}}>
