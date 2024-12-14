1fc65988a76c8e03977176859a483474ea1fc23d
Alex <alex.holder@bigfootds.com>
2024-12-14 22:26:48 +1100

    feat: conversion to astro start

--- src/contexts/ArticleTocContextSetup.js
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const ArticleTocContext = createContext(null);
-
-export function useArticleToc(){
-	return useContext(ArticleTocContext);
-}
-
